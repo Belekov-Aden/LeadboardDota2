c3455cbc90702a0f14df0b99b9bd5fbaafc47a34
B.Aden <belekovaden31@gmail.com>
2024-06-22 15:39:59 +1100

    chto to

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -11,6 +14,10 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/#{reqion}", response_class=HTMLResponse)
 def read_root(request: Request, reqion: str = 'europe'):
+    data: dict = requests.get(
+        f'https://www.dota2.com/webapi/ILeaderboard/GetDivisionLeaderboard/v0001?division={reqion}&leaderboard=0').json()
+
+    reqion = data.get('leaderboard')[0]
     return templates.TemplateResponse(
         request=request, name='main/main.html', context={"reqion": reqion}
     )
