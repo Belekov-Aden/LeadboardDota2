1b8741b3c6a5a4c11a2f9dcb73993525dce4fda9
B.Aden <belekovaden31@gmail.com>
2024-06-24 23:08:39 +1100

    fix css for mobile

--- test.py
+++ test.py
@@ -39,7 +39,7 @@ def write_data():
 
 
 def scheduler_thread():
-    schedule.every().day.at("20:28").do(write_data)
+    schedule.every().day.at("22:54").do(write_data)
     while True:
         schedule.run_pending()
         time.sleep(1)
